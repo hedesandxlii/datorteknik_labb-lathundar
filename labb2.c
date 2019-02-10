ea6581ab867e839e4e60fc9efb85db7011e68311
André <an7821he-s@student.lu.se>
2019-02-10 17:21:22 +0100

    Pulat lite, vi får se om det hjälper :/

--- labb2.c
+++ labb2.c
@@ -22,7 +22,7 @@ void set_period(uint16_t);
 void set_pulse(uint16_t);
 void pwm_stoff(uint16_t);
 void timer1_init();
-uint16_t uss_run();
+void uss_run();
 void set_pwm(uint16_t, uint16_t);
 
 int main(void)
@@ -34,12 +34,14 @@ int main(void)
 	timer1_init();
 	while (1)
 	{
-		set_pwm(uss_run()*255/(2^16-1), 0x00ff);
+		uss_run();
+		set_pwm(TCNT1*255/(2^16-1), 0x00ff);
 	}
 	
 }
 
-void statefull_button_pleb () {
+void statefull_button_pleb () 
+{
 	int *ledInit = 0x24; // set pointer to address of DDRB
 	int *ledData = 0x25; // set pointer to address of PORTB
 	int *buttonInit = 0x21; // set pointer to address of DDRA
@@ -61,7 +63,8 @@ void statefull_button_pleb () {
 	}
 }
 
-void statefull_button_pro () {
+void statefull_button_pro ()
+{
 	DDRB |= 0b00000001; // set data direction to output for led3
 	DDRD |= 0b00000000; // set data direction to input for buttons
 	
@@ -77,7 +80,8 @@ void statefull_button_pro () {
 	}
 }
 
-void statefull_button_proest () {
+void statefull_button_proest () 
+{
 	DDRB |= 0b00000001; // set data direction to output for led3
 	DDRD |= 0b00000000; // set data direction to input for buttons
 	
@@ -93,7 +97,8 @@ void statefull_button_proest () {
 	}
 }
 
-int button_read_reliability() {
+int button_read_reliability()
+{
 	/*
 		Idé: Hoppa över alla gropar och kolla värdet på första flanken och efter 50ms.
 		Vi har provat oss fram till 50ms.
@@ -108,7 +113,8 @@ int button_read_reliability() {
 	}
 }
 
-void usart_stoff() {
+void usart_stoff()
+{
 	usart0_init(103);
 	while(1){
 		usart0_transmit(usart0_receive());
@@ -143,7 +149,8 @@ void usart0_transmit( unsigned char data )
 	UDR0 = data;
 }
 
-void timer3_init() {
+void timer3_init()
+{
 	// Clear on comparison match.
 	TCCR3A |= (1<<COM3A1);
 	//TCCR3A |= (1<<COM3B1) | (0<<COM3B0);
@@ -164,19 +171,24 @@ void timer3_init() {
 	
 }
 
-void set_pulse(uint16_t arg) {
+void set_pulse(uint16_t arg)
+{
+	// Enl. s.156 måste den låga byten accessas först, men i C sköter compilern det.
 	OCR3A = arg;
-	//OCR3AH = arg >> 8;
 	//OCR3AL = arg;
+	//OCR3AH = arg >> 8;
 }
 
-void set_period(uint16_t ocr) {
+void set_period(uint16_t ocr)
+{
+	// Enl. s.156 måste den låga byten accessas först, men i C sköter compilern det.
 	ICR3 = ocr;
-	//ICR3H = ocr >> 8;
 	//ICR3L = ocr;
+	//ICR3H = ocr >> 8;
 }
 
-void pwm_stoff(uint16_t period) {
+void pwm_stoff(uint16_t period)
+{
 	timer3_init();
 	set_pulse(0);
 	set_period(period);
@@ -196,30 +208,36 @@ void pwm_stoff(uint16_t period) {
 	}
 }
 
-void set_pwm(uint16_t pulse, uint16_t period) {
-	OCR3A = pulse;
-	ICR3 = period;
+void set_pwm(uint16_t pulse, uint16_t period)
+{
+	set_pulse(pulse);
+	set_period(period);
 }
 
-void timer1_init() {
+void timer1_init()
+{
 	TCCR1B &= ~((1<<CS10)|(1<<CS11)|(1<<CS12));
 	/**/
 	DDRC |= (1<<PORTC1)|(0<<PINC0);
 }
 
-uint16_t uss_run() {
+void uss_run()
+{
+	// Clears timer before measurement
+	TCNT1 = 0;
 	// Creates a trigger pulse.
 	PORTC |= (1<<PORTC1);
 	_delay_us(10);
 	PORTC &= ~(1<<PORTC1);
+
 	// Waits for echo to go high
 	while(!PINC) {}
 	TCCR1B |= (1<<CS10);// Starts counter after echo goes high by setting prescaler value to 1.
 	
 	// Waits for echo to go low.
 	while(PINC) {}
-	TCCR1B &= ~((1<<CS10)|(1<<CS11)|(1<<CS12)); // Stops counter after echo goes low by removing prescaler value.
+	// Stops counter after echo goes low by removing prescaler value.
+	TCCR1B &= ~((1<<CS10)|(1<<CS11)|(1<<CS12)); 
 	
 	_delay_ms(60);
-	return TCNT1;
 }
